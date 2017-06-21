6812d41fa15ee66bf0c0cc2e075a5c383ddb0142
alexx150489 <nikolskiy150489@gmail.com>
2017-06-21 18:48:44 +0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+	$(".block-promo .reserve-btn").on("click", function(e) {
+		e.preventDefault();
+		var currentBlock = $(this).attr("href"),
+		currentBlockOffset = $(currentBlock).offset().top;
+		$("html, body").animate({
+			scrollTop: currentBlockOffset
+		}, 500);
+	});
 
 	$(".block-offers .reserve-btn").on("click", function(e) {
 		e.preventDefault();
